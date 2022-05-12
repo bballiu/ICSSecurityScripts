278b51197cfca5f1c85fc2dce4b84261f4ff96a4
benoit.balliu@howest.be <benoit.balliu@howest.be>
2022-05-12 09:35:57 +0200

    Robust demo file AllHigh (python practice)

--- DEMO/ModbusAllHigh.py
+++ DEMO/ModbusAllHigh.py
@@ -6,16 +6,43 @@
 from pyModbusTCP.client import ModbusClient
 from win_inet_pton import *
 import time
+import re 
 
 timeout=0.1
 
 def putHigh(c,i):
     c.write_single_coil(i,1)
+
+
+def validate_ip_address(address):
+    match = re.match(r"^(\d{1,3})\.(\d{1,3})\.(\d{1,3})\.(\d{1,3})$", address)
+
+    if bool(match) is False:
+        print("IP address {} is not valid".format(address))
+        sys.exit(1)
+        return False
+
+    for part in address.split("."):
+        if int(part) < 0 or int(part) > 255:
+            print("IP address {} is not valid".format(address))
+            sys.exit(1)
+            return False
+
+    print("IP address {} is valid".format(address))
+    return True 
     
 ## MAIN SHOW ##    
 #c = ModbusClient(host="172.20.2.30", port=502, auto_open=True, auto_close=True)
-c = ModbusClient(host="123.145.120.102", port=502, auto_open=True)
+try:
+    if validate_ip_address(sys.argv[1]) is False:
+        sys.exit(1)
+except IndexError:
+    print("Please provide an IP address as argument")
+    sys.exit(1)
 
+print("Opening connection...")
+c = ModbusClient(host=sys.argv[1], port=502, auto_open=True)
+print("Turning on outputs")
 for x in range(0,16):
         putHigh(c,x)
 
